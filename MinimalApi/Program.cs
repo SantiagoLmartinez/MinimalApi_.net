5895ba7c2456ab5ca94cc7f5984d9adc92382500
slm17 <slm17slm17@gmail.com>
2022-07-20 10:03:18 -0300

    primer commit

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,6 +1,8 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World 2!");
+
+//comentando el codigo
+app.MapGet("/", () => "Hello World!");
 
 app.Run();
