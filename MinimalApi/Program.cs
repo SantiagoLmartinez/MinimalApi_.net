cc2254cb1c04ead99a4474b995fe735a4b246245
slm17 <slm17slm17@gmail.com>
2022-07-20 21:29:40 -0300

    se conectan endpoints por metodo Get a 2 apis

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -3,6 +3,44 @@ var app = builder.Build();
 
 
 //comentando el codigo
-app.MapGet("/", () => "Hello World!1");
+app.MapGet("/", () => "Hello World!");
+
+// /hello?name=name
+app.MapGet("/hello", (string name) => $"Hola {name}");
+
+//le mandamos el dato por la url
+app.MapGet("/hellowithname/{name}/{lastname}",
+    (string name, string lastname) => $"Hola {name} {lastname}");
+//
+app.MapGet("/response", async () =>
+{
+    HttpClient client = new HttpClient();
+    var response = await client.GetAsync("https://jsonplaceholder.typicode.com/todos/");
+    response.EnsureSuccessStatusCode();
+    string responseBody = await response.Content.ReadAsStringAsync();
+    return responseBody;
+});
+
+//traer pokemon por su id
+app.MapGet("/pokemon/{id}", async (string id) =>
+{
+    HttpClient client = new HttpClient();
+    var response = await client.GetAsync($"https://pokeapi.co/api/v2/pokemon/{id}");
+    response.EnsureSuccessStatusCode();
+    string responseBody = await response.Content.ReadAsStringAsync();
+    return responseBody;
+});
+//traer pokemons 
+app.MapGet("/pokemon", async () =>
+{
+    HttpClient client = new HttpClient();
+    var response = await client.GetAsync("https://pokeapi.co/api/v2/pokemon/");
+    response.EnsureSuccessStatusCode();
+    string responseBody = await response.Content.ReadAsStringAsync();
+    return responseBody;
+});
+
+
 
 app.Run();
+  
